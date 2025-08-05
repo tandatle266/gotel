712de3039dac82f25b5d22f06d65649c183afc68
tandatle266 <tandatle266@gmail.com>
2025-08-05 14:41:10 +0700

    refactor for build

--- grpc/tracing.go
+++ grpc/tracing.go
@@ -3,22 +3,23 @@ package oteltracinggrpc
 import (
 	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
-func ServerOptions() []grpc.ServerOption {
-	return []grpc.ServerOption{
-		grpc.ChainUnaryInterceptor(
-			otelgrpc.UnaryServerInterceptor(),
-		),
-		grpc.ChainStreamInterceptor(
-			otelgrpc.StreamServerInterceptor(),
-		),
-	}
-}
+type ClientFactory[T any] func(cc grpc.ClientConnInterface) T
 
-func ClientOptions(serviceName string) []grpc.DialOption {
-	return []grpc.DialOption{
-		grpc.WithUnaryInterceptor(otelgrpc.UnaryClientInterceptor()),
-		grpc.WithStreamInterceptor(otelgrpc.StreamClientInterceptor()),
-	}
+// NewClientWithTracing create a gRPC client with tracing
+func NewClientWithTracing[T any](addr string, factory ClientFactory[T]) (client T, conn *grpc.ClientConn, err error) {
+    dialOptions := []grpc.DialOption{
+        grpc.WithTransportCredentials(insecure.NewCredentials()),
+        grpc.WithStatsHandler(otelgrpc.NewClientHandler()),
+    }
+
+    conn, err = grpc.Dial(addr, dialOptions...)
+    if err != nil {
+        return client, nil, err
+    }
+
+    client = factory(conn)
+    return client, conn, nil
 }
